2d214695966496a928b3e877d3afd9dcdb6ece19
Jonathan Ben Avraham <jonty@Jonathans-MacBook-Pro.local>
2023-10-04 14:58:36 +0100

    updated Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -7,7 +7,13 @@ from PIL import Image
 
 
 class SIJDataset(torch.utils.data.Dataset):
-    ''' INIT Function '''
+    '''
+        Init Function
+        path_to_labels: path to csv file containing the labels for each image
+        patients: the path to the numpy file containing all the patients
+        root_path: path to the images to be classified
+        transforms: the transforms to be applied to the images
+    '''
 
     def __init__(self, path_to_labels, patients, root_path, transform):
         self.labels = pd.read_csv(path_to_labels)
@@ -18,6 +24,16 @@ class SIJDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.patients)
 
+    '''
+        gets an image for a patient with a given index
+        loads the label for the given patient where the image filename is equal the the patient
+        the label is the 'target' variable from the csv where the patient is the same as the filename
+        the label is then assigned to the 0th index of a list
+        the file_path is set to the root_path / patient (filename)
+        the image is then loaded from the numpy array in the root_path
+        the transforms are applied to the image
+        finally, the transformed image and the label are returned
+    '''
     def __getitem__(self, idx):
         patient = self.patients[idx]
         data = self.labels[self.labels['image'] == patient]
