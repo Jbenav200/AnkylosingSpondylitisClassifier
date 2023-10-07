6391a5052d70efdd084291933e53153b879d176b
Jonathan Ben Avraham <jonty@Jonathans-MacBook-Pro.local>
2023-10-07 10:33:12 +0100

    updated comments in main.py

--- main.py
+++ main.py
@@ -26,57 +26,71 @@ MODEL_SAVE_PATH = os.getenv('MODEL_SAVE_PATH')
 SAVED_ENSEMBLE_PATH = os.getenv('SAVED_ENSEMBLE_PATH')
 
 if __name__ == '__main__':
-    # print('Program Started.')
+    print(f"Program started in {sys.argv[1]} mode.")
+    # set the device to GPU
     device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
 
+    # set the data transforms and load the datasets
     train_transforms, val_transforms = set_train_and_val_transforms()
     train_dataset = SIJDataset(LABELS_PATH, TRAIN_SUBJECTS, TRAIN_ROOT_PATH, transform=train_transforms)
     val_dataset = SIJDataset(LABELS_PATH, VAL_SUBJECTS, VAL_ROOT_PATH, transform=val_transforms)
 
+    # if sys.argv[1] is 'train', start the training process
     if sys.argv[1] == 'train':
         batch_size = 16
         num_workers = 8
 
+        # create the train and validation dataloaders.
         train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers,
                                                    shuffle=True)
         val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, num_workers=num_workers,
                                                  shuffle=False)
-
+        # create an instance of the modified ResNet18 model
         modelA = SIJResNet()
         modelA.to(device)
 
+        # create an instance of the modified DenseNet121 model
         modelB = SIJDenseNet()
         modelB.to(device)
 
+        # set up the callbacks for each model
         resnet_callback, densenet_callback, ensemble_callback = set_model_checkpoints()
 
+        # train the modified ResNet and DenseNet models
         train_model(modelA, RESNET_LOG_PATH, resnet_callback, train_loader, val_loader)
         train_model(modelB, DENSENET_LOG_PATH, densenet_callback, train_loader, val_loader)
 
+        # Load the modified ResNet18 model from the best model checkpoint
         test_model_a = SIJResNet.load_from_checkpoint(resnet_callback.best_model_path)
+        # send the model to GPU
         test_model_a.to(device)
+        # set the model to evaluate
         test_model_a.eval()
 
-        # # Print the ResNet18 classifier metrics
+        # Print the ResNet18 classifier metrics
         print_model_metrics(test_model_a, 'resnet model', device, val_dataset)
 
+        # load the modified DenseNet121 model from the best model checkpoint
         test_model_b = SIJDenseNet.load_from_checkpoint(densenet_callback.best_model_path)
+        # send the model to GPU
         test_model_b.to(device)
+        # set the model to evaluate
         test_model_b.eval()
-        #
-        # # Print the DenseNet121 classifier metrics
+
+        # Print the DenseNet121 classifier metrics
         print_model_metrics(test_model_b, 'densenet model', device, val_dataset)
 
         # train the SIJ Ensemble classifier
         model = SIJEnsemble(modelA, modelB, resnet_callback.best_model_path, densenet_callback.best_model_path)
         train_model(model, ENSEMBLE_LOG_PATH, ensemble_callback, train_loader, val_loader)
-        #
+
         model_test = SIJEnsemble.load_from_checkpoint(ensemble_callback.best_model_path)
         model_test.eval()
-        #
+
         # # print ensemble classifier metrics.
         print_model_metrics(model_test, 'ensemble classifier', device, val_dataset)
 
+        # if the second argument passed to the file is 'save' then save each of the models at their best checkpoints
         if sys.argv[2] is not None and sys.argv[2] == 'save':
             resnet = SIJResNet.load_from_checkpoint(resnet_callback.best_model_path)
             resnet.to(device)
@@ -87,6 +101,8 @@ if __name__ == '__main__':
             ensemble = SIJEnsemble.load_from_checkpoint(ensemble_callback.best_model_path)
             ensemble.to(device)
             torch.save(ensemble, MODEL_SAVE_PATH+'/ensemble.pt')
+    # if the first argument passed to the file is results, then analyse the performance of the ensemble classifier
+    # and print the results to the terminal.
     elif sys.argv[1] == 'results':
         try:
             ensemble = torch.load(SAVED_ENSEMBLE_PATH)
